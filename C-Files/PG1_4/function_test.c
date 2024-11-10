de91f56d463fa886975141ebe69604af69ed5e9e
Walpertinger <jehrmaier@web.de>
2024-11-10 16:03:33 +0100

    a

--- C-Files/PG1_4/function_test.c
+++ C-Files/PG1_4/function_test.c
@@ -1,28 +1,58 @@
 #include <stdio.h>
 
-float return_a_float(){
-    float a = 0.0;
-    printf("\nEnter a float value: ");
-    scanf("%f", &a);
-    return a;
+//Addiert 3 Zahlen
+int num_sum(int a, int b, int c){
+    
+    printf("\nPlease enter 3 integers for summation: \n");
+    scanf("%d %d %d", &a, &b, &c);
+
+    int sum = a + b + c;
+
+    printf("\n%d, %d and %d all together makes: %d\n", a, b, c, sum);
+    return 0;
 }
 
-int double_num(){
-    int a, b;
-    printf("\nGive two numbers for multiplication: ");
-    scanf("%d %d", &a, &b);
-    return a * b;
+//Subtrahiert 3 Zahlen
+int num_sub(int a, int b, int c){
+
+    printf("\nPlease enter 3 integers for subtraction: \n");
+    scanf("%d %d %d", &a, &b, &c);
+
+    int sub = a - b - c;
+
+    printf("\n%d minus %d and %d makes: %d\n", a, b, c, sub);
+    return 0;
 }
+
+//Multipliziert 2 Zahlen
+int num_multip(int a, int b){
+
+    printf("\nPlease enter 2 integers for multiplication: \n");
+    scanf("%d %d", &a, &b);
+
+    int multip = a * b;
+
+    printf("\n%d and %d multiplied makes: %d\n", a, b, multip);
+    return 0;
+}
+
+//Dividiert eine durch eine andere Zahl
+int num_div(int a, int b){
+
+    if (b==0) {
+        printf("\nno division with zero allowed!");
+    }
+
+    printf("\nPlease enter integer divident and devisor: \n");
+    scanf("%d %d", &a, &b);
+
+    int quotient = a / b;
+     
+    printf("\n%d devided by %d makes: %d\n", a, b, quotient);
+    return 0;
+}
+
 
 int main(){
-    float g = 0.0;
-    int z = 0;
-
-    g = return_a_float();
-    z = double_num();
-    
-    printf("\nThe value you entered is: %.2f\n", g);
-    printf("\nThe multiplication is: %d\n", z);
-    
     return 0;
 }
