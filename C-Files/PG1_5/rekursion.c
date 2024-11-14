8c00378298139c939fa4325fe1b2b34e580bd8e1
Walpertinger <jehrmaier@web.de>
2024-11-14 14:16:00 +0100

    a

--- C-Files/PG1_5/rekursion.c
+++ C-Files/PG1_5/rekursion.c
@@ -2,20 +2,20 @@
 
 long int factorial(long int n){
     static int i = 1;
-    if(i == 1) printf("\nBerechnung der Fakultät %ld: ", n);
+    if(i == 1) printf("\nBerechnung der Fakultaet %ld: ", n);
     
     if(n){
         i++;
         n >1 ? printf("%ld * ", n) : printf("%ld * 1", n);
         return n * factorial(n-1);
     }
-    printf("\n%d. und letzter Aufruf. ", i++);
+    printf("\n%d. und letzter Aufruf. ", i);
     return 1;
 }
 
 int main(){
-    int n = 0; 
+    int n = 5; 
 
-    printf("Die Fakultät von %d ist: %id",n,factorial(n));
+    printf("Die Fakultaet von %d ist: %ld",n,factorial(n));
     return 0;
 }
