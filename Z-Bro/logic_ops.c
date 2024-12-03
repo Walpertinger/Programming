14840b5e62d46ba1af8c2979b938e0a9c94078a4
Walpertinger <jehrmaier@web.de>
2024-12-03 20:23:27 +0100

    a

--- Z-Bro/logic_ops.c
+++ Z-Bro/logic_ops.c
@@ -2,10 +2,12 @@
 #include <stdbool.h>
 
 int main() {
-    /*
-    // AND operator
+    
     float temp = 25;
     bool sunny = true;
+
+    /*
+    // AND operator &&
 
     if(temp >= 0 && temp <= 30 && sunny){
         printf("\nThe weather is good!");
@@ -15,6 +17,28 @@ int main() {
     }
     */
     
+    /*
+    // OR operator ||
+
+    if(temp <= 0 || temp >= 30 || sunny == false){
+        printf("\nThe weather is bad! :(");
+    }
+    else{
+        printf("\nThe weather is good!");
+    }
+    */
+
     
+    // NOT operator !
+
+    if(!sunny){
+        printf("\nIt's cloudy outside!");
+    }
+    else{
+        printf("\nIt's sunny outside!");
+    }
+    
+    
+
     return 0;
 }
