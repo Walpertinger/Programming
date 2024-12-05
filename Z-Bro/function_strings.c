e396d011ec42544b2447bb44711c0b9105a4d11f
Walpertinger <jehrmaier@web.de>
2024-12-05 10:51:42 +0100

    a

--- Z-Bro/function_strings.c
+++ Z-Bro/function_strings.c
@@ -6,14 +6,40 @@ int main()
     char string1[] = "Julian";
     char string2[] = "Ehrmaier";
 
-    //strlwr(string1);
-    //strupr(string1);
-    //strcat(string1, string2);
-    //strncat(string1, string2, 1);
-    //strcpy(string1, string2);
-    //strncpy(string1, string2, 1);
+    //strlwr(string1);                              //converts everything lowercase
+    //strupr(string1);                              //converts everything uppercase
+    //strcat(string1, string2);                     //appends str2 to end of str1
+    //strncat(string1, string2, 1);                 //appends n characters from str2 to str1
+    //strcpy(string1, string2);                     //copy str2 to str1
+    //strncpy(string1, string2, 1);                 //copy n characters of str2 to str1
 
-    printf("%s", string1);
+    //strset(string1, '?');                         //sets all characters of a string to given character
+    //strnset(string1, 'x', 1);                     //sets first n characters of a string to a given character
+    //strrev(string1);                              //reverses a string
+
+    //printf("%s", string1);
+
+
+
+    //int result = strlen(string1);                 //returns string length as int
+    //int result = strcmp(string1, string2);        //string compare all characters 
+    //int result = strncmp(string1, string2, 1);    //string compare n characters
+    //int result = strcmpi(string1, string2);       //string compare all (ignore case)
+    //int result = stricmp(string1, string2, 1);    //string compare n characters (ignore case)
+
+    //printf("%d", result);
+
+
+    /*
+    if(result == 0)
+    {
+        printf("These strings are the same");
+    }
+    else
+    {
+        printf("These strings are not the same");
+    }
+    */
 
     return 0;
 }
