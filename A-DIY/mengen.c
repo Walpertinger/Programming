0856e9d6cde1f682cfe57e32021511b70917fc9c
Walpertinger <jehrmaier@web.de>
2024-12-31 18:30:41 +0100

    a

---
+++ A-DIY/mengen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    //test für Relation unterscheidung in zugehörigen Gruppen
+    //Benötigt sind die Menge A und die Menge der Relation selbst
+    int A[] = {1, 2, 3, 4};
+    int size_A = sizeof(A) / sizeof(A[0]);
+
+    printf("%d", size_A);
+    int B[5];
+
+
+
+    return 0;
+}
